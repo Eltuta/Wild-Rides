84613c8596f73e853a3934822ec20e8c773ffabf
Eltuta <amuors@yahoo.com>
2020-03-05 17:22:04 +0000

    configure cognito

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'eu-west-1_bqDDM0Vid', // e.g. eu-west-1_bqDDM0Vid
-        userPoolClientId: '1bt2vst0g0je4sjd4ba3jc9ejh', // e.g. 1bt2vst0g0je4sjd4ba3jc9ejh
+        userPoolId: 'eu-west-1_JtYfmbSSg', // e.g. eu-west-1_bqDDM0Vid
+        userPoolClientId: '718g5e462p5vo4gqdgfc0nokdf', // e.g. 1bt2vst0g0je4sjd4ba3jc9ejh
         region: 'eu-west-1' // e.g. us-east-2
     },
     api: {
