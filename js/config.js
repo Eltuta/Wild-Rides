c5295a8b44930b6caa75e0697d55a2422c939a7f
Eltuta <amuors@yahoo.com>
2020-03-05 16:50:07 +0000

    configure cognito

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. eu-west-1_bqDDM0Vid
-        userPoolClientId: '', // e.g. 1bt2vst0g0je4sjd4ba3jc9ejh
-        region: '' // e.g. us-east-2
+        userPoolId: 'eu-west-1_bqDDM0Vid', // e.g. eu-west-1_bqDDM0Vid
+        userPoolClientId: '1bt2vst0g0je4sjd4ba3jc9ejh', // e.g. 1bt2vst0g0je4sjd4ba3jc9ejh
+        region: 'eu-west-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
